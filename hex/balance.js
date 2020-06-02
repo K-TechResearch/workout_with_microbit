2698eb8e1378a8367ed02f75ad18bc14791a9a59
toshi-tahara <dummy@example.com>
2020-06-03 02:06:10 +0900

    重力加速度が1023一定でないため，加速度の変動での計算に変更。但しメモリエラー

--- hex/balance.js
+++ hex/balance.js
@@ -6,23 +6,37 @@ input.onButtonPressed(Button.A, function () {
     basic.showNumber(1)
     start = input.runningTime()
     end = start + GAMETIME
+    flagSTART = 0
+    tempdiff = 0
     while (end > input.runningTime()) {
         countprog()
     }
     flagA = 1
     count = Math.round(count / (GAMETIME / JUDGETIME))
-    count = count - 0
-    MAX = MAX - 0
 })
-function countprog() {
-    count += Math.abs(input.acceleration(Dimension.Strength) - 1023)
-    MAX = Math.max(MAX, Math.abs(input.acceleration(Dimension.Strength) - 1023))
-    basic.pause(JUDGETIME)
+function countprog () {
+    if (flagSTART == 0) {
+        tempvalue = input.acceleration(Dimension.Strength)
+        flagSTART = 1
+        配列.push(tempdiff)
+        basic.pause(JUDGETIME)
+    } else {
+        tempdiff = Math.abs(input.acceleration(Dimension.Strength) - tempvalue)
+        tempvalue = input.acceleration(Dimension.Strength)
+        count += tempdiff
+        MAX = Math.max(MAX, tempdiff)
+        配列.push(tempdiff)
+        basic.pause(JUDGETIME)
+    }
 }
 input.onButtonPressed(Button.B, function () {
-    basic.showNumber(input.acceleration(Dimension.Strength))
+    serial.writeNumbers(配列)
 })
 let MAX = 0
+let 配列: number[] = []
+let tempvalue = 0
+let tempdiff = 0
+let flagSTART = 0
 let end = 0
 let start = 0
 let flagA = 0
